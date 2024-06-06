830b4439776988b75bc8d063c812de5a5438b438
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-06-06 13:35:51 -0700

    Establish node program

---
+++ index.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+// Now I need to "Add the code needed to serve the right pafe according to the url"
+
+// I don't understand this instruction^^^ Soo starting the lesson back a few steps
+
+
+// Here's where we're at. Starting with step 1 in getting started: 
+// We've installed node. 
+// We can run node by running the terminal command: 
+// node index.js
+
+// ^^ Doing this tells the shell(?) to run our script with node. 
+
+// We'll also embed this information with a "shebang" line at the top of the file
+// This line explicitly gives the absolute path of the interpreter. 
+// Not all operating systems have node in the bin folder, but all should have env. So we use env node
+// 
+
+
+console.log('Hello World');
