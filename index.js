be0ab9a6f45422c2cbca02f8d5de2600a654245a
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-06-06 17:28:06 -0700

    Reorganize notes

--- index.js
+++ index.js
@@ -1,42 +1,37 @@
 #!/usr/bin/env node
 
-// Now I need to "Add the code needed to serve the right pafe according to the url"
+// Here's where we're at. Starting with step 1 in getting started:
+// We've installed node.
 
-// I don't understand this instruction^^^ Soo starting the lesson back a few steps
-
-
-// Here's where we're at. Starting with step 1 in getting started: 
-// We've installed node. 
-// We can run node by running the terminal command: 
+//  ************************************************************************************************************************************
+// We can run node by running the terminal command:
 // node index.js
+// ^^ Doing this tells the shell(?) to run our script with node.
+// Alternately we could use the watch flag to watch for changes:
+// node --watch index.js
+// control+c to quit
+//  ************************************************************************************************************************************
 
-// ^^ Doing this tells the shell(?) to run our script with node. 
-
-// We'll also embed this information with a "shebang" line at the top of the file
-// This line explicitly gives the absolute path of the interpreter. 
+// We'll embed this information with a "shebang" line at the top of the file
+// This line explicitly gives the absolute path of the interpreter.
 // Not all operating systems have node in the bin folder, but all should have env. So we use env node
 // Now when we run this program, it logs hello world in our terminal console.
 // It does not log hello world in the console of index.html, because we have not linked this as our script
-
-
-console.log('Hello World');
+console.log("Hello Node console");
 
 // Next, while in the same directory that contains our node file, we run the following command in the terminal:
 // chmod u+x index.js
-// This gives our file permission to use the shebang file?. Idk. but we'll move on. 
+// This gives our file permission to use the shebang file?. Idk. but we'll move on.
 
-// Now we can pass strings in the terminal as javascript. Idk why yet.... But yeah. 
+//  ************************************************************************************************************************************
+// Now we can pass strings in the terminal as javascript. Idk why yet.... But yeah.
 // We use syntax:
 // node -e ""
 // ^^Your javascript(for example a console.log statement) between the ""
+//  ************************************************************************************************************************************
 
-
-// Use watch to continuously watch for changes and restart the file:
-// node --watch index.js
-// control + c to quit
-
-// Next we're trying to access the environment variables set at the time process was started. 
-// We run the following to set these two vaiables: 
+// Next we're trying to access the environment variables set at the time process was started.
+// We run the following to set these two vaiables:
 // USER_ID=239482 USER_KEY=foobar node index.js
 
 // console.log(process.env.USER_ID);
@@ -44,32 +39,38 @@ console.log('Hello World');
 // console.log(USER_ID);
 
 // I have no idea what this is on about... I only get undefined when I try to console.log(process.....)
+// Moving on...
 
+//  ************************************************************************************************************************************
 
-var http = require('http');
+const http = require("http");
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello mutherfucking World!');
-}).listen(8080);
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello mutherfucking World!");
+  })
+  .listen(8080);
 
-
-// Ok whatever this^^ did it worked. We can see our message at the following url: 
+// Ok whatever this^^ did it worked. We can see our message at the following url:
 // http://localhost:8080/
 // So I believe we've created a server and asked  node to print some plain text on that server?
 // In order for that^ page to be viewable Node must be running. Either run "node index.js" or "node --watch index.js"
 
+//  ************************************************************************************************************************************
+// Next we Create a separate module file and created a module to export
 
-// Next we Created a separate module file and created a module to export
+let dateModule = require("./modules/myFirstModule.js");
 
-let dateModule = require('./modules/myFirstModule.js');
-
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/plain" });
     res.write("The date and time is currently: " + dateModule.myDateTime());
     res.end();
-}).listen(8020);
+  })
+  .listen(8020);
 
 // Now we're getting the date and time from a separate module and displaying it at localHost:8020
 
-
+//  ************************************************************************************************************************************
+// Next exercise:
