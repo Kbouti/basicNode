b6ada1fc81282470dda932426ae2a9a6e98d5f70
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-06-08 19:19:40 -0700

    Serve appropriate html file as requested by the client

--- script.js
+++ script.js
@@ -205,15 +205,45 @@ console.log("Hello Node console");
 // Next up is the built in URL module.
 // This is used to break a web address up into readable parts
 
+// const url = require("url");
+// const adr = "http://localhost:8080/default.htm?year=2017&month=february";
+// const q = url.parse(adr, true);
+
+// console.log(q.host); //returns 'localhost:8080'
+// console.log(q.pathname); //returns '/default.htm'
+// console.log(q.search); //returns '?year=2017&month=february'
+
+// const qdata = q.query; //returns an object: { year: 2017, month: 'february' }
+// console.log(qdata.month); //returns 'february'
+
+//  ************************************************************************************************************************************
+// Now we know how to parse the string, and in the previous chapter we learned how to make Node.js behave as a file server.... (right?)
+// So up next we combine them to serve the file requested by the client. We're asked to create two html files, summer and winter
+// Done. They live in modules
+
+// Now we create a now file that opens the requested file and returns the content to the client. If anything goes wrong throw an error
+
+const http = require("http");
+const fs = require("fs");
 const url = require("url");
-const adr = "http://localhost:8080/default.htm?year=2017&month=february";
-const q = url.parse(adr, true);
 
-console.log(q.host); //returns 'localhost:8080'
-console.log(q.pathname); //returns '/default.htm'
-console.log(q.search); //returns '?year=2017&month=february'
+http
+  .createServer(function (req, res) {
+    const q = url.parse(req.url, true);
+    const filename = "./modules/" + q.pathname;
+    fs.readFile(filename, function (err, data) {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        return res.end("404 not found");
+      }
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      return res.end();
+    });
+  })
+  .listen(8080);
 
-const qdata = q.query; //returns an object: { year: 2017, month: 'february' }
-console.log(qdata.month); //returns 'february'
-
+// Boom!!! We've done something that resembles what we're being asked to do for the project.  Hooray!
+// We can now in theory host a site with multiple pages.
+//  ************************************************************************************************************************************
 //  ************************************************************************************************************************************
